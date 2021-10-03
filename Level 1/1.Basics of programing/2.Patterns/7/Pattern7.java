407d58564e37515e91cbd6b0d869c13dd7331226
Junaid Qureshi <Junaid.562000@gmail.com>
2021-10-03 20:09:21 +0530

    Q.7 Solved

--- Level 1/1.Basics of programing/2.Patterns/7/Pattern7.java
+++ Level 1/1.Basics of programing/2.Patterns/7/Pattern7.java
@@ -5,14 +5,15 @@ public class Pattern7 {
         Scanner scn  = new Scanner(System.in);
         int n = scn.nextInt();
         for(int i=1;i<=n;i++){
-            for(int j=1;j<i;j++){
-            System.out.print("\t");
-        }   
-        System.out.print("*\t");
-           for(int j=n;j>i;j--){
-            System.out.print("\t");
-        } 
-        System.out.println();
+            for(int j=1;j<=n;j++){
+                if(i==j){
+                    System.out.print("*\t");
+                }
+                else{
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
         }
         }
 }
